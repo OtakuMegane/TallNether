9854ae1d861894ab26f583b00ea776e9552cd782
OtakuMegane <otakumegane@insaneotaku.com>
2018-07-18 11:32:53 -0400

    Dropping pre-1.12 support

---
+++ v1_13_R1/src/main/java/com/minefit/xerxestireiron/tallnether/v1_13_R1/TallNether_WorldGenDecoratorNetherGlowstone.java
@@ -0,0 +1,29 @@
+package com.minefit.xerxestireiron.tallnether.v1_13_R1;
+
+import java.util.Random;
+
+import net.minecraft.server.v1_13_R1.BlockPosition;
+import net.minecraft.server.v1_13_R1.ChunkGenerator;
+import net.minecraft.server.v1_13_R1.GeneratorAccess;
+import net.minecraft.server.v1_13_R1.GeneratorSettings;
+import net.minecraft.server.v1_13_R1.WorldGenDecorator;
+import net.minecraft.server.v1_13_R1.WorldGenDecoratorFrequencyConfiguration;
+import net.minecraft.server.v1_13_R1.WorldGenFeatureConfiguration;
+import net.minecraft.server.v1_13_R1.WorldGenerator;
+
+public class TallNether_WorldGenDecoratorNetherGlowstone extends WorldGenDecorator<WorldGenDecoratorFrequencyConfiguration> {
+
+    public TallNether_WorldGenDecoratorNetherGlowstone() {}
+
+    public <C extends WorldGenFeatureConfiguration> boolean a(GeneratorAccess generatoraccess, ChunkGenerator<? extends GeneratorSettings> chunkgenerator, Random random, BlockPosition blockposition, WorldGenDecoratorFrequencyConfiguration worldgendecoratorfrequencyconfiguration, WorldGenerator<C> worldgenerator, C c0) {
+        for (int i = 0; i < random.nextInt(random.nextInt(worldgendecoratorfrequencyconfiguration.a) + 1); ++i) {
+            int j = random.nextInt(16);
+            int k = random.nextInt(248) + 4;
+            int l = random.nextInt(16);
+
+            worldgenerator.generate(generatoraccess, chunkgenerator, random, blockposition.a(j, k, l), c0);
+        }
+
+        return true;
+    }
+}
