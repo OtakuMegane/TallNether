4eb0b70e3ef43fa907fb3fc8f25809c564793445
OtakuMegane <otakumegane@insaneotaku.com>
2018-10-22 19:20:27 -0400

    And let's make our special version detection actually work...

--- TallNether/src/main/java/com/minefit/xerxestireiron/tallnether/ManageHell.java
+++ TallNether/src/main/java/com/minefit/xerxestireiron/tallnether/ManageHell.java
@@ -1,5 +1,6 @@
 package com.minefit.xerxestireiron.tallnether;
 
+import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.configuration.ConfigurationSection;
 
@@ -9,7 +10,7 @@ public class ManageHell {
     private final World world;
     private com.minefit.xerxestireiron.tallnether.v1_12_R1.LoadHell LH12R1;
     private com.minefit.xerxestireiron.tallnether.v1_13_R1.LoadHell LH13R1;
-    private com.minefit.xerxestireiron.tallnether.v1_13_R2_2.LoadHell LH13R2;
+    private com.minefit.xerxestireiron.tallnether.v1_13_R2.LoadHell LH13R2;
     private com.minefit.xerxestireiron.tallnether.v1_13_R2_2.LoadHell LH13R2_2;
 
     public ManageHell(World world, TallNether instance) {
@@ -25,11 +26,11 @@ public class ManageHell {
             this.LH13R1 = new com.minefit.xerxestireiron.tallnether.v1_13_R1.LoadHell(this.world, worldConfig,
                     this.plugin.getName());
         } else if (this.plugin.version.equals("v1_13_R2")) {
-            if (this.plugin.serverVersion.getRevision().equals("2")) {
+            if (Bukkit.getVersion().contains("1.13.2")) {
                 this.LH13R2_2 = new com.minefit.xerxestireiron.tallnether.v1_13_R2_2.LoadHell(this.world, worldConfig,
                         this.plugin.getName());
             } else {
-                this.LH13R2 = new com.minefit.xerxestireiron.tallnether.v1_13_R2_2.LoadHell(this.world, worldConfig,
+                this.LH13R2 = new com.minefit.xerxestireiron.tallnether.v1_13_R2.LoadHell(this.world, worldConfig,
                         this.plugin.getName());
             }
 
@@ -44,7 +45,7 @@ public class ManageHell {
         } else if (this.plugin.version.equals("v1_13_R1")) {
             this.LH13R1.restoreGenerator();
         } else if (this.plugin.version.equals("v1_13_R2")) {
-            if (this.plugin.serverVersion.getRevision().equals("2")) {
+            if (Bukkit.getVersion().contains("1.13.2")) {
                 this.LH13R2_2.restoreGenerator();
             } else {
                 this.LH13R2.restoreGenerator();
